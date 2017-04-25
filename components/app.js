a8314e771e137bc9a6a36a4416cea1a77fcf0b14
philippe Do <philippe Do>
2017-04-25 05:48:11 +0100

    completed part 5

--- components/app.js
+++ components/app.js
@@ -1,8 +1,14 @@
 import React, { Component } from 'react'
+import TextInput from './TextInput'
 
 class App extends Component {
     render() {
-        return <div>This is definitely a (module reloading) React app now!</div>
+        return (
+            <div>
+                <h1>This is the App Component</h1>
+                <TextInput/>
+            </div>
+        )
     }
 }
 
