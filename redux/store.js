f80f7e43a12105b5caf5838d4f6e603ca537e084
TRAVCO\pdonnette <pdonnette@travco.co.uk>
2017-04-26 15:04:13 +0100

    completed part 11

--- redux/store.js
+++ redux/store.js
@@ -1,11 +1,12 @@
 import { applyMiddleware, compose, createStore } from 'redux'
 import rootReducer from './reducers'
 import { createLogger } from 'redux-logger'
+import thunk from 'redux-thunk'
 
 let finalCreateStore = compose(
-    applyMiddleware(createLogger())
+    applyMiddleware(thunk, createLogger())
 )(createStore)
 
-export default function configureStore(initialState = { todos: [] }) {
+export default function configureStore(initialState = { todos: [], user: {} }) {
     return finalCreateStore(rootReducer, initialState)
 }
