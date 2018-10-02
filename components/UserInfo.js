753f931ab571562b46911624c490286f8fc5d6b5
philippe.donnette <philippe.donnette@gmail.com>
2018-10-02 20:42:40 +0300

    quick change for nothing

--- components/UserInfo.js
+++ components/UserInfo.js
@@ -19,6 +19,7 @@ class UserInfo extends Component {
             <li>
                 <div>username: {this.props.user.username}</div>
                 <div>id: {this.props.user.id}</div>
+                <div>Just a quick change for nothing</div>
                 <button onClick={this.handleNewId.bind(this)}>Update with random ID</button>
                 <button onClick={this.handleNewIdIfOdd.bind(this)}>Update only if odd</button>
                 <button onClick={this.handleNewIdAsync.bind(this)}>Update async</button>
